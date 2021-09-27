c92986dcab25fc575ecdda7492a3c87ee95b8b9b
yosefkirkcohen <ycohen45@yahoo.com>
2021-09-27 13:40:18 -0700

    added route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react'
 import SearchPage from './SearchPage'
 import './App.css'
+import Home from './Home.js'
+import {
+  BrowserRouter as Router, 
+  Route, 
+  Switch,
+} from 'react-router-dom';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <SearchPage />
-      </div>
+                <Router>
+                    <Switch>
+                        <Route 
+                            path="/" 
+                            exact
+                            render={(routerProps) => <Home {...routerProps} />} 
+                        />
+                        <Route 
+                            path="/searchPage" 
+                            exact
+                            render={(routerProps) => <SearchPage {...routerProps} />} 
+                        />
+                        {/* <Route 
+                          path="/users/:myId" 
+                          exact
+                          render={(routerProps) => <DetailPage {...routerProps} />} 
+                        /> */}
+                    </Switch>
+                </Router>
+            </div>
     )
   }
 }
